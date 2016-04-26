01891f428a768ab39558bdf075cf148f56108fb3
Trevor Sullivan <pcgeek86@gmail.com>
2016-04-25 20:09:21 -0600

    Added documentation

--- Azure-Infrastructure/azuredeploy.ps1
+++ Azure-Infrastructure/azuredeploy.ps1
@@ -1,3 +1,4 @@
+
 function ConvertFrom-SecureStringToPlainText {
     [CmdletBinding()]
     param ( 
@@ -6,7 +7,6 @@ function ConvertFrom-SecureStringToPlainText {
 
     $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
     return [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-
 }
 
 $ErrorActionPreference = 'Stop';
@@ -18,10 +18,17 @@ $VerbosePreference = 'continue';
 
 ### The Start-AzureRm command is part of the Azure PowerShell Extensions project.
 ### On PowerShell version 5.0+, use Install-Module -Name AzureExt -Scope CurrentUser to install it
+if (!(Get-Command -Name Install-Module -ErrorAction Ignore)) {
+  throw 'Please make sure you''re using PowerShell 5.0 or later.';
+}
+Install-Module -Name AzureExt -Scope CurrentUser;
+
+### Authenticates to Microsoft Azure and caches the authentication token locally
 Start-AzureRm;
 
 Write-Verbose -Message 'Creating Resource Group ...';
 
+### Create an Azure Resource Manager (ARM) Resource Group
 $ResourceGroup = @{
     Name = 'MMS2016-Infrastructure2';
     Location = 'North Central US';
@@ -33,6 +40,7 @@ New-AzureRmResourceGroup @ResourceGroup;
 Write-Verbose -Message 'Finished creating Azure Resource Group.';
 Write-Verbose -Message 'Creating ARM JSON template deployment ...';
 
+### Create a declarative Azure Resource Manager (ARM) JSON Template deployment inside the Resource Group
 $Deployment = @{
     ResourceGroupName = $ResourceGroup.Name;
     Name = 'MMS2016-Infrastructure';
